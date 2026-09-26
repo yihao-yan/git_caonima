d25c062f75693a54cc329ba6a991c3ee8aed52a3
yanyihao <3559166539Qqq.com>
2026-09-26 11:26:24 +0800

    ceyixia

--- dw/ada.c
+++ dw/ada.c
@@ -4,7 +4,7 @@ int main(){
     int b = 20;
     int c = a + b;
     printf("c = %d\n", c);
-    getchar();
+    getchar();  printf("Hello World\n");  printf("Hello World\n");
     return 0;
        printf("c = %d\n", c);
  }
